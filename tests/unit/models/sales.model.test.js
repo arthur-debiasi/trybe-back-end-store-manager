ada07f4e848d75fc914ec3cb0a7ff54112416074
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-01-17 10:23:34 -0300

    feat: register sale model test

---
+++ tests/unit/models/sales.model.test.js
@@ -0,0 +1,38 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+const { salesModel } = require("../../../src/models");
+
+const connection = require("../../../src/models/connection");
+const { salesList, noProductIdSale, validSale, validSaleResponse } = require("./mocks/sales.model.mock");
+
+describe("Testes de unidade do model de vendas", function () {
+  afterEach(function () {
+    sinon.restore();
+  })
+  describe("Testando a listagem de vendas", function () {
+    it("de todas as vendas", async function () {
+      sinon.stub(connection, "execute").resolves([salesList]);
+
+      const result = await salesModel.listSales();
+
+      expect(result).to.be.deep.equal(salesList);
+    });
+
+    it("de uma venda a partir do seu id", async function () {
+      sinon.stub(connection, "execute").resolves([salesList[0]]); 
+      
+      const result = await salesModel.listSalesById(1);
+
+      expect(result).to.be.deep.equal(salesList[0]);
+    });
+  });
+  describe("Testando o registro de vendas", function () {
+    it('de uma venda válida', async function () {
+      sinon.stub(connection, "execute").resolves([{ insertId: 4 }]);
+      
+      const response = await salesModel.registerSales(validSale);
+      
+      expect(response).to.be.deep.equal(4);
+    })
+  });
+});
